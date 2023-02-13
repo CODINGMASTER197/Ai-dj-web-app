f219f9bd52ed1e7c7633cb2f2152410d45e19986
CODINGMASTER197 <100692426+CODINGMASTER197@users.noreply.github.com>
2023-02-13 19:32:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
 
 function preload(){
 song = loadSound("music.mp3");
@@ -26,6 +27,8 @@ function modelLoaded(){
 function gotposes(results){
     if(results.length > 0 ){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -42,10 +45,24 @@ function gotposes(results){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals/500
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
+    
+
 }
 
 function play(){
     song.play();
-    song.setvolume(1);
+    song.setVolume(1);
     song.rate(1);
 }
